c6b430cd632339160c08466b695747f4cf17a27b
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 14:17:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,9 +9,3 @@ def happy_birthday(birthday_kids)
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 end
-
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
