cd302220ed89a00040558aac5456f75819329a74
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 14:18:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,14 @@
 
 def select_winner(passengers)
   
-    passengers.each do |suite, name|
+    winner = ""
+passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
 
 winner
-end 	
+
+end
+
